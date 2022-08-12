96d12c82b02f571baf355634b7c18b3ade94b8e1
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-08-12 07:18:30 +0000

    book controller記載

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def show
@@ -7,4 +15,14 @@ class BooksController < ApplicationController
 
   def index
   end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
