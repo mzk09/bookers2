0bfeedf28e31e534773e6845e3e690bed1e0d61a
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-08-17 12:20:24 +0000

    user edit作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+      devise_parameter_sanitizer.permit(:account_update,keys: [:name, :introduction])
     end
 
 end
