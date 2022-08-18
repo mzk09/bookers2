9284c4bddc6f29cc7d10ca44ae7a78c0cfda740a
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-08-18 14:28:28 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only:[:index,:show,:edit,:update]
-  before_action :ensure_user, only:[:edit,:update,:destroy]
+  before_action :correct_user, only:[:edit,:update]
 
   def show
     @user = User.find(params[:id])
@@ -34,10 +34,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name,:profile_image,:introduction)
   end
 
-  def ensure_user
-    if @user = current_user
-      redirect_to user_path
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user.id) unless @user == current_user
   end
 
 end
